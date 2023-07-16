42f9c68e4b29fb31a5174f82002b08f63dd37551
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-07-17 04:16:02 +0600

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div className='pt-10 bg-[#0a192f] text-Footer'>
+        <div className='pt-10 bg-[#0a192f] text-Footer font-Bitter'>
             <footer className="footer pb-1 py-5 space-x-3 footer-center rounded">
                 <div>
                     {/* <div className="grid grid-flow-col gap-4">
@@ -22,11 +22,11 @@ const Footer = () => {
                             Developed by <Link className='text-green-500' to="https://github.com/TanjimulSabbir" target='_blank'>Tanjimul Islam Sabbir.</Link>
                             </small>
                             <div className='flex justify-center items-center space-x-2'>
-                                <Link className='list-none text-[9px] text-white' to="https://github.com/TanjimulSabbir"><FiGithub /> </Link>
-                                <Link className='list-none text-[9px] text-white' to="/https://www.linkedin.com/in/TanjimulSabbir"><FiLinkedin /> </Link>
-                                <Link className='list-none text-[9px] text-white' to="/https://www.facebook.com/tanjimulsabbir.brahminykite"><FiFacebook /> </Link>
+                                <Link className='list-none text-[10px] text-white' to="https://github.com/TanjimulSabbir"><FiGithub /> </Link>
+                                <Link className='list-none text-[10px] text-white' to="/https://www.linkedin.com/in/TanjimulSabbir"><FiLinkedin /> </Link>
+                                <Link className='list-none text-[10px] text-white' to="/https://www.facebook.com/tanjimulsabbir.brahminykite"><FiFacebook /> </Link>
 
-                                <Link className='list-none text-[9px] text-white' to="/https://www.discord.com/tanjimulsabbir"><  BsDiscord /> </Link>
+                                <Link className='list-none text-[10px] text-white' to="/https://www.discord.com/tanjimulsabbir"><  BsDiscord /> </Link>
                             </div>
                     </div>
                 </div>
--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -3,19 +3,18 @@ import { Link } from "react-router-dom";
 function NotFound() {
     return (
         <div className="bg-no-repeat bg-cover" style={{ backgroundImage: `url('https://img.freepik.com/premium-photo/two-raw-striploin-steak-new-york-beef-board-top-view-black-stone-background_187166-54697.jpg?w=996')` }}>
-        <div className='w-full h-screen flex justify-center items-center bg-black bg-opacity-70 font-Bitter' >
-            
-            <div className='flex flex-col justify-center items-center'>
-            <div className='flex flex-col justify-center items-center'>
-                <h2 className='text-2xl uppercase text-white font-Lato'>Page Not Found</h2>
-            </div>
-                <p className='w-2/3 mt-3 text-center text-sm text-gray-100 font-lora'>The page are you looking for might have been doesnt exist or had its name changed or temporarily unavailable.</p>
-                <Link to="http://localhost:5173/meal-counter" className="mt-6 font-Roboto text-green-500 cursor-pointer rounded-full border border-green-500 p-4 uppercase transition duration-500 hover:bg-green-500 hover:text-black hover:border-green-500" >Go to home Page
-                </Link>
-            </div>
-        </div>
-        </div>
+            <div className='w-full h-screen flex justify-center items-center bg-black bg-opacity-70 font-Bitter' >
 
+                <div className='flex flex-col justify-center items-center'>
+                    <div className='flex flex-col justify-center items-center'>
+                        <h2 className='text-2xl uppercase text-white font-Lato'>Page Not Found</h2>
+                    </div>
+                    <p className='w-2/3 mt-3 text-center text-sm text-gray-100 font-lora'>The page are you looking for might have been doesnt exist or had its name changed or temporarily unavailable.</p>
+                    <Link to="http://localhost:5173/meal-counter" className="mt-6 font-Roboto text-green-500 cursor-pointer rounded-full border border-green-500 p-4 uppercase transition duration-500 hover:bg-green-500 hover:text-black hover:border-green-500" >Go to home Page
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
