fb835d4f9a7a5e6d03c6b8e59ef6f8ddb0d845b7
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-07-17 03:40:24 +0600

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import { FiGithub, FiLinkedin, FiFacebook } from 'react-icons/fi';
-import { CiTwitter } from 'react-icons/ci';
+import { BsDiscord } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
@@ -8,27 +8,27 @@ const Footer = () => {
             <footer className="footer pb-1 py-5 space-x-3 footer-center rounded">
                 <div>
                     {/* <div className="grid grid-flow-col gap-4">
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Rooms</a>
-                    <a className="link link-hover">Press kit</a>
+                    <Link className="link link-hover">About us</Link>
+                    <Link className="link link-hover">Contact</Link>
+                    <Link className="link link-hover">Rooms</Link>
+                    <Link className="link link-hover">Press kit</Link>
                 </div> */}
 
                     <div className=''>
-                        {/* <div className='mid-lg:fixed mid-lg:bottom-32 mid-lg:left-14'>
-                            <div className='flex justify-center items-center space-x-5'>
-                                <a className='list-none text-xs text-white' href="/#"><FiGithub /> </a>
-                                <a className='list-none text-xs text-white' href="/#"><FiLinkedin /> </a>
-                                <a className='list-none text-xs text-white' href="/#"><FiFacebook /> </a>
-                                <a className='list-none text-xs text-white' href="/#"><CiTwitter /> </a>
-                            </div>
-                        </div> */}
                         <div>
                             <p>Copyright © 2023 - All rights reserved by Rangon House. <small>
                             Developed by <Link className='text-green-500' to="https://github.com/TanjimulSabbir" target='_blank'>Tanjimul Islam Sabbir.</Link>
                             </small>
                             </p>
+                            <div className='flex justify-center items-center space-x-2'>
+                                <Link className='list-none text-xs text-white' to="https://github.com/TanjimulSabbir"><FiGithub /> </Link>
+                                <Link className='list-none text-xs text-white' to="/https://www.linkedin.com/in/TanjimulSabbir"><FiLinkedin /> </Link>
+                                <Link className='list-none text-xs text-white' to="/https://www.facebook.com/tanjimulsabbir.brahminykite"><FiFacebook /> </Link>
+
+                                <Link className='list-none text-xs text-white' to="/https://www.discord.com/tanjimulsabbir"><  BsDiscord /> </Link>
+                            </div>
                         </div>
+                      
                     </div>
                 </div>
             </footer>
