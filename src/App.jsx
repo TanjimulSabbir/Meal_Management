acd1f18ed75d1de167f9ede37f918134eae66328
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-07-17 04:13:29 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login'
 import Home from "../src/components/Home/Home"
 import PrivateRoute from './Layout/Shared/PrivateRoute';
+import NotFound from './components/NotFound/NotFound';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path='/meal-counter' element={<PrivateRoute><Home></Home></PrivateRoute>}></Route>
       <Route path='/' element={<PrivateRoute><Home></Home></PrivateRoute>}></Route>
+      <Route path='*' element={<NotFound></NotFound>}/>
     </Routes>
   );
 };
